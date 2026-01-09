d16a91e0493d19bb6be794e8ec2c7bb29fa3b40c
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-09 21:06:34 +0700

    Update mixer.h

--- include/mixer.h
+++ include/mixer.h
@@ -1,15 +1,11 @@
 #pragma once
 
 typedef struct {
-    float roll;
-    float pitch;
-    float yaw;
-    float throttle;
-} control_t;
-
-typedef struct {
     float left;
     float right;
-} elevon_t;
+} mixer_output_t;
 
-elevon_t mixer_elevon(control_t in);
+void Mixer_Elevon(float throttle,
+                  float roll,
+                  float pitch,
+                  mixer_output_t *out);

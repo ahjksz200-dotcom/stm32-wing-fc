24f81492fc6cc03a6f1e90eb2ec48e1d2fe731a2
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-09 21:05:16 +0700

    Update imu.cpp

--- src/imu.cpp
+++ src/imu.cpp
@@ -1,29 +1,9 @@
 #include "imu.h"
-#include "stm32f4xx_hal.h"
 
-static float ax, ay, az;
-static float gx, gy, gz;
+void IMU_Init(void) {}
 
-void imuInit() {
-    MPU6500_Init();
-}
-
-void imuUpdate() {
-    int16_t raw_ax, raw_ay, raw_az;
-    int16_t raw_gx, raw_gy, raw_gz;
-
-    MPU6500_ReadRaw(&raw_ax,&raw_ay,&raw_az,&raw_gx,&raw_gy,&raw_gz);
-
-    ax = raw_ax * 0.000488f;
-    ay = raw_ay * 0.000488f;
-    az = raw_az * 0.000488f;
-
-    gx = raw_gx * 0.061f;
-    gy = raw_gy * 0.061f;
-    gz = raw_gz * 0.061f;
-}
-
-void imuGet(float* acc, float* gyro) {
-    acc[0]=ax; acc[1]=ay; acc[2]=az;
-    gyro[0]=gx; gyro[1]=gy; gyro[2]=gz;
+void IMU_Update(imu_data_t *imu) {
+    imu->gyro_x = 0;
+    imu->gyro_y = 0;
+    imu->gyro_z = 0;
 }

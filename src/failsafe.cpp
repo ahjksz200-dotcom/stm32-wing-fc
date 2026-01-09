4893fbd6428f6f6cd34aee66723d50f5e35022c5
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-09 12:42:50 +0700

    Update failsafe.cpp

--- src/failsafe.cpp
+++ src/failsafe.cpp
@@ -1,5 +1,24 @@
 #include "failsafe.h"
 
-bool failsafeActive() {
-    return rcLost();
+static failsafe_state_t fs;
+
+void Failsafe_Init(void)
+{
+    fs.rc_lost  = false;
+    fs.gps_lost = false;
+    fs.active   = false;
+}
+
+void Failsafe_Update(bool rc_ok, bool gps_ok)
+{
+    fs.rc_lost  = !rc_ok;
+    fs.gps_lost = !gps_ok;
+
+    // failsafe kích hoạt nếu mất RC
+    fs.active = fs.rc_lost;
+}
+
+failsafe_state_t Failsafe_GetState(void)
+{
+    return fs;
 }

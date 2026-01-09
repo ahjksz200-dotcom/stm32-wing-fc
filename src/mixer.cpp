e9670c7bae196a421c857199f80f516a54f6fcba
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-09 21:07:27 +0700

    Update mixer.cpp

--- src/mixer.cpp
+++ src/mixer.cpp
@@ -1,10 +1,9 @@
 #include "mixer.h"
 
-void mixerElevon(float throttle, float roll, float pitch,
-                 float* left, float* right) {
-    *left  = throttle + pitch + roll;
-    *right = throttle + pitch - roll;
-
-    if(*left>1) *left=1;
-    if(*right>1)*right=1;
+void Mixer_Elevon(float throttle,
+                  float roll,
+                  float pitch,
+                  mixer_output_t *out) {
+    out->left  = throttle + pitch + roll;
+    out->right = throttle + pitch - roll;
 }

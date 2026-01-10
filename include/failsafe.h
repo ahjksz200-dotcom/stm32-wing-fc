e647558a1540d2002180d01c68e7b436f8a4ed01
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-10 21:28:23 +0700

    Update failsafe.h

--- include/failsafe.h
+++ include/failsafe.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <stdbool.h>
 
-void FAILSAFE_Init(void);
-void FAILSAFE_Trigger(void);
-bool FAILSAFE_IsActive(void);
+void failsafe_trigger(void);
+bool failsafe_active(void);

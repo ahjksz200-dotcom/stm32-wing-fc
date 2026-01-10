e41f6cfce3b48cd4018d0e09842db6bb84f72141
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-10 19:22:36 +0700

    Update fc.h

--- include/fc.h
+++ include/fc.h
@@ -1,5 +1,13 @@
 #pragma once
-#include <stdint.h>
+#include "stm32f4xx_hal.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void FC_Init(void);
 void FC_Loop(void);
+
+#ifdef __cplusplus
+}
+#endif

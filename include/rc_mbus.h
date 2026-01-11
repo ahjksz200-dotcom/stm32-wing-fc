ad151f64b4107237d8377d44053d80744672539e
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 15:50:53 +0700

    Update rc_mbus.h

--- include/rc_mbus.h
+++ include/rc_mbus.h
@@ -1,5 +1,22 @@
 #pragma once
 #include <stdint.h>
+#include <stdbool.h>
+
+#define MBUS_CHANNELS 8
+
+typedef struct {
+    uint16_t ch[MBUS_CHANNELS];
+    bool failsafe;
+} mbus_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void MBUS_Init(void);
 uint8_t MBUS_ReadRaw(uint16_t *channels);
+bool MBUS_Failsafe(void);
+
+#ifdef __cplusplus
+}
+#endif

de97cb749076c3e71bb1eab10713cda1a81638be
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 18:38:59 +0700

    Update imu.h

--- include/imu.h
+++ include/imu.h
@@ -6,6 +6,16 @@ extern "C" {
 #endif
 
 typedef struct {
+    /* RAW SENSOR */
+    int16_t gyro_x;
+    int16_t gyro_y;
+    int16_t gyro_z;
+
+    int16_t acc_x;
+    int16_t acc_y;
+    int16_t acc_z;
+
+    /* ATTITUDE (deg * 100) */
     int16_t roll;
     int16_t pitch;
     int16_t yaw;

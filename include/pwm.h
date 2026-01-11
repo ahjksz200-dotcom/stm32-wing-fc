dbd2d698403b8743fdecfa31c9a8c8c410399e64
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 19:15:51 +0700

    Update pwm.h

--- include/pwm.h
+++ include/pwm.h
@@ -1,20 +1,27 @@
-#ifndef PWM_H
-#define PWM_H
-
+#pragma once
 #include <stdint.h>
 
-typedef enum {
+/* Logical PWM channels */
+typedef enum
+{
+    PWM_ESC = 0,
+    PWM_SERVO_L,
+    PWM_SERVO_R,
+    PWM_AUX1
+} pwm_channel_t;
+
+/* Low-level timer channels */
+typedef enum
+{
     PWM_CH1 = 0,
     PWM_CH2,
     PWM_CH3,
     PWM_CH4
 } pwm_ch_t;
 
+/* API */
 void PWM_Init(void);
-void PWM_SetMicroseconds(pwm_ch_t ch, uint16_t us);
+void PWM_Write(pwm_channel_t ch, uint16_t us);
 
-/* ESC control */
 void PWM_ArmESC(void);
 void PWM_DisarmESC(void);
-
-#endif

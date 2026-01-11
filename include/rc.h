b17bc4ffa5a5c5caf01a6e9b6f24b3e6a1ced68e
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 16:21:59 +0700

    Update rc.h

--- include/rc.h
+++ include/rc.h
@@ -5,17 +5,16 @@ typedef struct {
     int16_t roll;
     int16_t pitch;
     int16_t yaw;
-    int16_t throttle;
+    uint16_t throttle;
     uint8_t arm;
-    uint8_t mode;
 } rc_data_t;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-uint8_t RC_Read(rc_data_t *out);
 void RC_Init(void);
+uint8_t RC_Read(rc_data_t *out);
 
 #ifdef __cplusplus
 }

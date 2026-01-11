845685999d7893faf4a4774d5ac9313e6793ccee
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 19:45:02 +0700

    Update mixer.cpp

--- src/mixer.cpp
+++ src/mixer.cpp
@@ -1,12 +1,13 @@
 #include "mixer.h"
-#include "pid.h"
 #include "pwm.h"
 
 void mixer_elevon(const pid_output_t *pid, uint16_t throttle)
 {
-    int16_t left  = 1500 + pid->roll + pid->pitch;
-    int16_t right = 1500 - pid->roll + pid->pitch;
+    int16_t left = 1500 + pid->roll + pid->pitch;  // Tính toán tín hiệu cho servo trái
+    int16_t right = 1500 - pid->roll + pid->pitch; // Tính toán tín hiệu cho servo phải
 
+    // Gửi tín hiệu PWM tới servo và ESC
     PWM_Write(PWM_SERVO_L, left);
     PWM_Write(PWM_SERVO_R, right);
+    PWM_Write(PWM_ESC, throttle); // Điều khiển ESC (throttle)
 }

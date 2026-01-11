a9b231b3062170a0f2ca3ab55a11d0bfe54cfa7d
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 18:58:22 +0700

    Update pwm.h

--- include/pwm.h
+++ include/pwm.h
@@ -1,17 +1,16 @@
-#pragma once
+#ifndef PWM_H
+#define PWM_H
+
 #include <stdint.h>
 
 typedef enum {
-    PWM_ESC = 0,
-    PWM_SERVO_L,
-    PWM_SERVO_R,
-    PWM_CH_MAX
+    PWM_CH1 = 0,
+    PWM_CH2,
+    PWM_CH3,
+    PWM_CH4
 } pwm_ch_t;
 
-#define PWM_MIN_US 1000
-#define PWM_MAX_US 2000
-
 void PWM_Init(void);
-void PWM_ArmESC(void);
-void PWM_DisarmESC(void);
-void PWM_Write(pwm_ch_t ch, uint16_t us);
+void PWM_SetMicroseconds(pwm_ch_t ch, uint16_t us);
+
+#endif

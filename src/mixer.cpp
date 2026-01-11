1909a2b95f6e5c29208540c7797dc956734f12a4
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 17:59:46 +0700

    Update mixer.cpp

--- src/mixer.cpp
+++ src/mixer.cpp
@@ -1,18 +1,10 @@
-#include "mixer.h"
 #include "pwm.h"
 
 void mixer_elevon(const pid_output_t *pid, uint16_t throttle)
 {
-    int16_t left, right;
+    int16_t left  = 1500 + pid->roll + pid->pitch;
+    int16_t right = 1500 - pid->roll + pid->pitch;
 
-    left  = throttle + pid->pitch - pid->roll;
-    right = throttle + pid->pitch + pid->roll;
-
-    if (left < PWM_MIN_US)  left = PWM_MIN_US;
-    if (left > PWM_MAX_US)  left = PWM_MAX_US;
-    if (right < PWM_MIN_US) right = PWM_MIN_US;
-    if (right > PWM_MAX_US) right = PWM_MAX_US;
-
-    PWM_SetMicroseconds(PWM_CH2, left);
-    PWM_SetMicroseconds(PWM_CH3, right);
+    PWM_Write(PWM_SERVO_L, left);
+    PWM_Write(PWM_SERVO_R, right);
 }

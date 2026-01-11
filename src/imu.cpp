5f54332f3f48b07f68335e66f9e4906a2a92f736
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 18:40:00 +0700

    Update imu.cpp

--- src/imu.cpp
+++ src/imu.cpp
@@ -2,21 +2,21 @@
 
 void IMU_Init(void)
 {
-    // TODO: init MPU6500
+    // TODO: init I2C/SPI + MPU6500
 }
 
 void IMU_Update(imu_data_t *imu)
 {
-    // TẠM THỜI: giả lập để build & test FC loop
+    /* Fake data for now */
     imu->gyro_x = 0;
     imu->gyro_y = 0;
     imu->gyro_z = 0;
 
     imu->acc_x = 0;
     imu->acc_y = 0;
-    imu->acc_z = 1.0f;
+    imu->acc_z = 16384; // 1G
 
-    imu->roll  = 0.0f;
-    imu->pitch = 0.0f;
-    imu->yaw   = 0.0f;
+    imu->roll  = 0;
+    imu->pitch = 0;
+    imu->yaw   = 0;
 }

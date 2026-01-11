f700d9a989f60b6e10699b914b0097e587eef572
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 13:36:23 +0700

    Update imu.cpp

--- src/imu.cpp
+++ src/imu.cpp
@@ -1,9 +1,18 @@
 #include "imu.h"
 
-void IMU_Init(void) {}
+void IMU_Init(void)
+{
+    // TODO: init MPU6500 (I2C/SPI)
+}
 
-void IMU_Update(imu_data_t *imu) {
+void IMU_Update(imu_data_t *imu)
+{
+    // TẠM THỜI MOCK DATA – build pass trước
     imu->gyro_x = 0;
     imu->gyro_y = 0;
     imu->gyro_z = 0;
+
+    imu->acc_x = 0;
+    imu->acc_y = 0;
+    imu->acc_z = 16384; // ~1G
 }

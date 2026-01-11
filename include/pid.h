082dfa97763a1fbbc5fb5f8c0365d4f2eff1aba3
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 14:10:04 +0700

    Update pid.h

--- include/pid.h
+++ include/pid.h
@@ -1,13 +1,16 @@
 #pragma once
+#include <stdint.h>
 #include "imu.h"
-#include "rc_mbus.h"
+#include "rc.h"
 
 typedef struct {
-    int16_t roll;
-    int16_t pitch;
+    float roll;
+    float pitch;
+    float yaw;
 } pid_output_t;
 
-void PID_Init(void);
-void PID_Update(const imu_data_t *imu,
-                const rc_data_t *rc,
-                pid_output_t *out);
+void PID_Update(
+    const imu_data_t *imu,
+    const rc_data_t  *rc,
+    pid_output_t     *out
+);

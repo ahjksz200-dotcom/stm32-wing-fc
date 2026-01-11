e884a192aea6c67f6e376f57a31fcb06e9f8663c
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 18:59:44 +0700

    Update pwm.cpp

--- src/pwm.cpp
+++ src/pwm.cpp
@@ -1,31 +1,18 @@
 #include "pwm.h"
 #include "timer_pwm.h"
 
-void PWM_Init(void)
+void PWM_SetMicroseconds(pwm_ch_t ch, uint16_t us)
 {
-    MX_TIM3_Init();
+    if (us < 1000) us = 1000;
+    if (us > 2000) us = 2000;
 
-    HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
-    HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
-    HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
-}
+    uint32_t ccr = us; // nếu timer chạy 1MHz (1us = 1 tick)
 
-void PWM_SetMicroseconds(pwm_channel_t ch, uint16_t us)
-{
-    if (us < PWM_MIN_US) us = PWM_MIN_US;
-    if (us > PWM_MAX_US) us = PWM_MAX_US;
-
-    switch (ch) {
-        case PWM_CH1:
-            __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, us);
-            break;
-        case PWM_CH2:
-            __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, us);
-            break;
-        case PWM_CH3:
-            __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_3, us);
-            break;
-        default:
-            break;
+    switch (ch)
+    {
+        case PWM_CH1: __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, ccr); break;
+        case PWM_CH2: __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, ccr); break;
+        case PWM_CH3: __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_3, ccr); break;
+        case PWM_CH4: __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_4, ccr); break;
     }
 }

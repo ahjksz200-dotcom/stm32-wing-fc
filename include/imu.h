3a0bfb0ffd2f2b6b8e9dd502d5dba91301ca9eee
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 18:27:31 +0700

    Update imu.h

--- include/imu.h
+++ include/imu.h
@@ -1,23 +1,15 @@
 #pragma once
 #include <stdint.h>
 
-typedef struct {
-    float roll;
-    float pitch;
-    float yaw;
-
-    float gyro_x;
-    float gyro_y;
-    float gyro_z;
-
-    float acc_x;
-    float acc_y;
-    float acc_z;
-} imu_data_t;
-
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef struct {
+    int16_t roll;
+    int16_t pitch;
+    int16_t yaw;
+} imu_data_t;
 
 void IMU_Init(void);
 void IMU_Update(imu_data_t *imu);

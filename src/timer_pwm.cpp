d66ff2c03e202392dc5357c3c90b35b2d951242e
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 18:52:19 +0700

    Update timer_pwm.cpp

--- src/timer_pwm.cpp
+++ src/timer_pwm.cpp
@@ -1,6 +1,5 @@
-#include "stm32f4xx_hal.h"
+#include "timer_pwm.h"
 
-/* TIM3 dùng cho PWM */
 TIM_HandleTypeDef htim3;
 
 void MX_TIM3_Init(void)
@@ -8,9 +7,9 @@ void MX_TIM3_Init(void)
     __HAL_RCC_TIM3_CLK_ENABLE();
 
     htim3.Instance = TIM3;
-    htim3.Init.Prescaler = 84 - 1;        // 84MHz / 84 = 1MHz (1us)
+    htim3.Init.Prescaler = 84 - 1;
     htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-    htim3.Init.Period = 20000 - 1;        // 20ms (50Hz)
+    htim3.Init.Period = 20000 - 1;
     htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
     htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
 
@@ -18,7 +17,7 @@ void MX_TIM3_Init(void)
 
     TIM_OC_InitTypeDef sConfigOC = {};
     sConfigOC.OCMode = TIM_OCMODE_PWM1;
-    sConfigOC.Pulse = 1500;               // trung tâm
+    sConfigOC.Pulse = 1500;
     sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
     sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
 

9897aaa4315d117dd2b7e6c19c27f35004308e21
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 14:05:30 +0700

    Update output.cpp

--- src/output.cpp
+++ src/output.cpp
@@ -1,10 +1,17 @@
 #include "output.h"
+#include "pwm.h"
 
-void outputInit() {
+uint16_t esc = 1000;
+uint16_t servoL = 1500;
+uint16_t servoR = 1500;
+
+void outputInit()
+{
     pwmInit();
 }
 
-void outputUpdate() {
+void outputUpdate()
+{
     pwmWrite(0, esc);
     pwmWrite(1, servoL);
     pwmWrite(2, servoR);

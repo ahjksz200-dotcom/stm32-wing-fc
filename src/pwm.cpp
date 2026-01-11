40ac175b4642c982e785e2ca01dc6740dfe05d72
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 18:53:10 +0700

    Update pwm.cpp

--- src/pwm.cpp
+++ src/pwm.cpp
@@ -1,51 +1,31 @@
 #include "pwm.h"
-#include "stm32f4xx_hal.h"
-
-extern TIM_HandleTypeDef htim3;
-
-static uint32_t pwm_channel_map[PWM_CH_MAX] = {
-    TIM_CHANNEL_1, // ESC
-    TIM_CHANNEL_2, // Servo L
-    TIM_CHANNEL_3  // Servo R
-};
-
-static inline uint32_t us_to_ccr(uint16_t us)
-{
-    // TIM chạy 1 MHz → 1us = 1 count
-    return us;
-}
+#include "timer_pwm.h"
 
 void PWM_Init(void)
 {
+    MX_TIM3_Init();
+
     HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
     HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
     HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
-
-    PWM_DisarmESC();
 }
 
-void PWM_ArmESC(void)
+void PWM_SetMicroseconds(pwm_channel_t ch, uint16_t us)
 {
-    PWM_Write(PWM_ESC, PWM_MIN_US);
-}
-
-void PWM_DisarmESC(void)
-{
-    PWM_Write(PWM_ESC, PWM_MIN_US);
-    PWM_Write(PWM_SERVO_L, 1500);
-    PWM_Write(PWM_SERVO_R, 1500);
-}
-
-void PWM_Write(pwm_ch_t ch, uint16_t us)
-{
-    if (ch >= PWM_CH_MAX) return;
-
     if (us < PWM_MIN_US) us = PWM_MIN_US;
     if (us > PWM_MAX_US) us = PWM_MAX_US;
 
-    __HAL_TIM_SET_COMPARE(
-        &htim3,
-        pwm_channel_map[ch],
-        us_to_ccr(us)
-    );
+    switch (ch) {
+        case PWM_CH1:
+            __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, us);
+            break;
+        case PWM_CH2:
+            __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, us);
+            break;
+        case PWM_CH3:
+            __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_3, us);
+            break;
+        default:
+            break;
+    }
 }

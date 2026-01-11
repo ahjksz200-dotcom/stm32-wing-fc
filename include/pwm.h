19392bf15b667825415eed9998658eb0df4f761d
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 17:52:34 +0700

    Update pwm.h

--- include/pwm.h
+++ include/pwm.h
@@ -1,27 +1,17 @@
 #pragma once
-#include "stm32f4xx_hal.h"
+#include <stdint.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+typedef enum {
+    PWM_ESC = 0,
+    PWM_SERVO_L,
+    PWM_SERVO_R,
+    PWM_CH_MAX
+} pwm_ch_t;
 
 #define PWM_MIN_US 1000
 #define PWM_MAX_US 2000
-#define PWM_MID_US 1500
-
-typedef enum {
-    PWM_CH1 = 0,   // ESC
-    PWM_CH2,       // Servo Left
-    PWM_CH3,       // Servo Right
-    PWM_CH4,
-    PWM_CH_TOTAL
-} pwm_channel_t;
 
 void PWM_Init(void);
-void PWM_SetMicroseconds(pwm_channel_t ch, uint16_t us);
 void PWM_ArmESC(void);
 void PWM_DisarmESC(void);
-
-#ifdef __cplusplus
-}
-#endif
+void PWM_Write(pwm_ch_t ch, uint16_t us);

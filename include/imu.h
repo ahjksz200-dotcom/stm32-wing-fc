160bb1226119c31d5070aa47c655fb8eac62ab3b
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 13:38:26 +0700

    Update imu.h

--- include/imu.h
+++ include/imu.h
@@ -1,10 +1,24 @@
 #pragma once
 #include "stm32f4xx_hal.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct {
-    int16_t ax, ay, az;
-    int16_t gx, gy, gz;
+    int16_t gyro_x;
+    int16_t gyro_y;
+    int16_t gyro_z;
+
+    int16_t acc_x;
+    int16_t acc_y;
+    int16_t acc_z;
 } imu_data_t;
 
+/* API */
 void IMU_Init(void);
-void IMU_Read(imu_data_t *imu);
+void IMU_Update(imu_data_t *imu);
+
+#ifdef __cplusplus
+}
+#endif

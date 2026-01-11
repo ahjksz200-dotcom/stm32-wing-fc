43119b71682c55cb8ba6b7d41e04bdd755004be3
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-11 16:19:02 +0700

    Update rc.cpp

--- src/rc.cpp
+++ src/rc.cpp
@@ -1,19 +1,25 @@
 #include "rc.h"
 #include "rc_mbus.h"
 
+void RC_Init(void)
+{
+    MBUS_Init();
+}
+
 uint8_t RC_Read(rc_data_t *out)
 {
-    uint16_t ch[8];
+    uint16_t ch[MBUS_CHANNELS];
 
-    if (!MBUS_ReadRaw(ch))
+    if (!MBUS_ReadRaw(ch)) {
         return 0;
+    }
 
-    out->roll     = ch[0] - 1500;
-    out->pitch    = ch[1] - 1500;
-    out->yaw      = ch[2] - 1500;
+    out->roll     = (int16_t)ch[0] - 1500;
+    out->pitch    = (int16_t)ch[1] - 1500;
+    out->yaw      = (int16_t)ch[2] - 1500;
     out->throttle = ch[3];
-    out->arm      = (ch[4] > 1500);
-    out->mode     = (ch[5] - 1000) / 500;
+
+    out->arm = (ch[4] > 1500);
 
     return 1;
 }

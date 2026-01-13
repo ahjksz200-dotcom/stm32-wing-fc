35fcb4f716a746b87d52a24b32be1772d25a9ef5
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-13 13:42:28 +0700

    Update rc_mbus.h

--- include/rc_mbus.h
+++ include/rc_mbus.h
@@ -3,7 +3,15 @@
 #include <stdbool.h>
 
 #define MBUS_CHANNELS 16
+#define MBUS_FRAME_LEN 25
 
+typedef struct
+{
+    uint16_t ch[MBUS_CHANNELS];
+    bool failsafe;
+} mbus_t;
+
+/* API */
 void MBUS_Init(void);
-bool MBUS_Update(void);              // gọi trong loop
-uint16_t MBUS_Read(uint8_t ch);      // 0–15
+uint8_t MBUS_ReadRaw(uint16_t *channels);
+bool MBUS_Failsafe(void);

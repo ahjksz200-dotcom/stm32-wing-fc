a5ac243dce8e0bbcf2abca4e7b413a72eace733c
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-18 13:53:24 +0700

    Update rc.cpp

--- src/rc.cpp
+++ src/rc.cpp
@@ -1,18 +1,25 @@
 #include "rc.h"
+#include "rc_mbus.h"
 
 void RC_Init(void)
 {
-    /* chưa làm gì – để build */
+    MBUS_Init();
 }
 
 uint8_t RC_Read(rc_data_t *rc)
 {
-    /* giá trị giả để test FC */
-    rc->throttle = 1000;
-    rc->roll     = 1500;
-    rc->pitch    = 1500;
-    rc->yaw      = 1500;
-    rc->arm      = 0;
+    uint16_t ch[MBUS_CHANNELS];
 
-    return 1; // RC OK
+    if (!MBUS_ReadRaw(ch))
+        return 0;
+
+    /* Map chuẩn RC */
+    rc->roll     = ch[0];
+    rc->pitch    = ch[1];
+    rc->throttle = ch[2];
+    rc->yaw      = ch[3];
+
+    rc->arm = (ch[4] > 1500);  // AUX1
+
+    return 1;
 }

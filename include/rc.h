3c9ecc130bb7bc69e65b30ad98db7b14944ce2c8
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-18 14:12:59 +0700

    Update rc.h

--- include/rc.h
+++ include/rc.h
@@ -1,24 +1,21 @@
 #pragma once
 #include <stdint.h>
+#include <stdbool.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+/* ===== RC CONFIG ===== */
+#define RC_CHANNELS 14
 
-/* ===== RC DATA (STUB – TẠM THỜI) ===== */
 typedef struct
 {
-    uint16_t throttle;
+    uint16_t ch[RC_CHANNELS];
+
     uint16_t roll;
     uint16_t pitch;
     uint16_t yaw;
-    uint8_t  arm;
+    uint16_t throttle;
+
 } rc_data_t;
 
 /* ===== API ===== */
 void RC_Init(void);
-uint8_t RC_Read(rc_data_t *rc);
-
-#ifdef __cplusplus
-}
-#endif
+bool RC_Read(rc_data_t *rc);

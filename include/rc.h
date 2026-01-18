ecc74344f2b2adcbe97ea1bc67c95171fe73f2ab
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-18 13:08:36 +0700

    Update rc.h

--- include/rc.h
+++ include/rc.h
@@ -1,18 +1,12 @@
 #pragma once
-#include <stdint.h>
 
-/* RC channel data (chuẩn FC) */
-typedef struct
-{
-    uint16_t roll;
-    uint16_t pitch;
-    uint16_t yaw;
-    uint16_t throttle;
-    uint16_t aux1;
-    uint16_t aux2;
-    uint8_t  arm;
-} rc_data_t;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-/* API */
 void RC_Init(void);
-uint8_t RC_Read(rc_data_t *out);
+void RC_Read(void);
+
+#ifdef __cplusplus
+}
+#endif

6315ed32c3588a270c9dcf914b6b2f936ab601e9
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-18 14:09:01 +0700

    Update uart.h

--- include/uart.h
+++ include/uart.h
@@ -1,4 +1,11 @@
 #pragma once
 #include "stm32f4xx_hal.h"
+#include <stdint.h>
+#include <stdbool.h>
 
-extern UART_HandleTypeDef huart1;
+/* ===== MBUS UART ===== */
+void UART_Init_MBUS(void);
+bool UART_ReadByte_MBUS(uint8_t *b);
+
+/* HAL callback */
+void USART1_IRQHandler(void);

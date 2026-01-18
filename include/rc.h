74e8b5f746c968d41b7d3751197d67ddbf509185
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-18 13:36:42 +0700

    Update rc.h

--- include/rc.h
+++ include/rc.h
@@ -1,11 +1,23 @@
 #pragma once
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/* ===== RC DATA (STUB – TẠM THỜI) ===== */
+typedef struct
+{
+    uint16_t throttle;
+    uint16_t roll;
+    uint16_t pitch;
+    uint16_t yaw;
+    uint8_t  arm;
+} rc_data_t;
+
+/* ===== API ===== */
 void RC_Init(void);
-void RC_Read(void);
+uint8_t RC_Read(rc_data_t *rc);
 
 #ifdef __cplusplus
 }

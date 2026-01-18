95c42cbdecc966ce925de9edf392dbafe000c319
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-18 14:14:39 +0700

    Update rc_mbus.h

--- include/rc_mbus.h
+++ include/rc_mbus.h
@@ -1,17 +1,8 @@
 #pragma once
 #include <stdint.h>
 #include <stdbool.h>
+#include "rc.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#define MBUS_CHANNELS 16
-
-void MBUS_Init(void);
-uint8_t MBUS_ReadRaw(uint16_t *ch);
-bool MBUS_Failsafe(void);
-
-#ifdef __cplusplus
-}
-#endif
+/* ===== MBUS ===== */
+void RC_MBUS_Init(void);
+bool RC_MBUS_Read(rc_data_t *rc);

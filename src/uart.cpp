3d3e3877f2ece8da210736a1a0890e032abe57bc
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-01-18 13:57:16 +0700

    Update uart.cpp

--- src/uart.cpp
+++ src/uart.cpp
@@ -1,35 +1,18 @@
 #include "uart.h"
-#include "stm32f4xx_hal.h"
 
 UART_HandleTypeDef huart1;
 
-void UART_Init_MBUS(void)
+void UART_Init(void)
 {
     __HAL_RCC_USART1_CLK_ENABLE();
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-
-    GPIO_InitTypeDef gpio = {0};
-
-    gpio.Pin = GPIO_PIN_9 | GPIO_PIN_10;
-    gpio.Mode = GPIO_MODE_AF_PP;
-    gpio.Pull = GPIO_NOPULL;
-    gpio.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    gpio.Alternate = GPIO_AF7_USART1;
-    HAL_GPIO_Init(GPIOA, &gpio);
 
     huart1.Instance = USART1;
-    huart1.Init.BaudRate = 115200;
+    huart1.Init.BaudRate   = 115200;   // MBUS
     huart1.Init.WordLength = UART_WORDLENGTH_8B;
-    huart1.Init.StopBits = UART_STOPBITS_1;
-    huart1.Init.Parity = UART_PARITY_NONE;
-    huart1.Init.Mode = UART_MODE_RX;
-    huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-    huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+    huart1.Init.StopBits   = UART_STOPBITS_1;
+    huart1.Init.Parity     = UART_PARITY_NONE;
+    huart1.Init.Mode       = UART_MODE_RX;
+    huart1.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
 
     HAL_UART_Init(&huart1);
-}
-
-uint8_t UART_ReadByte_MBUS(uint8_t *byte)
-{
-    return (HAL_UART_Receive(&huart1, byte, 1, 0) == HAL_OK);
 }
